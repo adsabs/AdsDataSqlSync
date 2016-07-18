cff82d6fae73ad9803490ffd8bed35519d387b39
SpacemanSteve <Stephen.McDonald@cfa.harvard.edu>
2016-07-18 13:25:16 -0400

    #10 cleaned up delta code use sql select statement to find changes between two different row views

--- scripts/fromDelta.sh
+++ scripts/fromDelta.sh
@@ -1,20 +1,26 @@
 
-if [ "$#" -ne 4 ]; then
+if [ "$#" -ne 5 ]; then
     echo 'create database schema, load column files, create row view and populate metrics database'
-    echo 'usage: fromScratch sqlSyncSchemaName metricsSchemaName baselineSchemaName deltaSchemaName'
+    echo 'usage: fromDelta sqlSyncSchemaName metricsSchemaName dataPath pythonPath baselineSchema'
     exit 1
 fi
 
 # create database tables and load column files
-psql -h localhost -U postgres -v v1=$1 -f ../sqlSync/scripts/createSqlSyncTables.sql
-psql -h localhost -U postgres -v v1=$1 -f ../sqlSync/scripts/loadColumnFiles.sql
+echo "creating database tables"
+#psql -h localhost -U postgres -v v1=$1 -f ../sqlSync/scripts/createSqlSyncTables.sql
+#psql -h localhost -U postgres -v rowViewSchema=$1 -v dataRoot=$3 -v pythonRoot=$4 -f ../sqlSync/scripts/loadColumnFiles.sql
+
+echo "loading column files"
+#psql -h localhost -U postgres -v v1=$1 -f ../sqlSync/scripts/loadColumnFiles.sql
 
 # create row view
-psql -h localhost -U postgres -v v1=$1 -f ../sqlSync/scripts/createSqlSyncRowView.sql
+echo "creating row view"
+#psql -h localhost -U postgres -v v1=$1 -f ../sqlSync/scripts/createSqlSyncRowView.sql
 
-psql -h localhost -U postgres -v v1=$1 v2=$3 v3=$4 -f ../sqlSync/createSqlSyncChangedView.sql
+echo "creating changed view"
+psql -h localhost -U postgres -v v1=$1 -v v2=$5  -f ../sqlSync/scripts/createSqlSyncChangedView.sql
 
 #psql -h localhost -U postgres -v v1=$2 -f ../metrics/scripts/createMetricsTable.sql
 
 # update all bibcodes in metrics database
-python ../metrics/metrics.py metricsCompute -rowViewSchema $1 -metricsSchema $2 --deltaSchema $4
+#python ../metrics/metrics.py metricsCompute -rowViewSchema $1 -metricsSchema $2 --deltaSchema $4
--- sqlSync/scripts/createSqlSyncChangedView.sql
+++ sqlSync/scripts/createSqlSyncChangedView.sql
@@ -1,22 +1,22 @@
 
-drop materialized view if exists :v3.ChangedRows;
+drop materialized view if exists :v1.ChangedRowsM;
 
-create materialized view :v3.ChangedRows as 
-       select :v1.RowView.bibcode 
-       from :v1.RowView,:v2.RowView 
-       where :v1.RowView.bibcode=:v2.RowView.bibcode 
-          and :v1.RowView.authors!=:v2.RowView.authors 
-	  or :v1.RowView.refereed!=:v2.RowView.refereed 
-	  or :v1.RowView.simbad_objects!=:v2.RowView.simbad_objects 
-	  or :v1.RowView.grants!=:v2.RowView.grants 
-	  or :v1.RowView.citations!=:v2.RowView.citations 
-	  or :v1.RowView.boost!=:v2.rowView.boost 
-	  or :v1.RowView.norm_cites!=:v2.RowView.norm_cites 
-	  or :v1.RowView.citation_count!=:v2.RowView.citation_count 
-	  or :v1.RowView.read_count!=:v2.RowView.read_count 
-	  or :v1.RowView.readers!=:v2.RowView.readers 
-	  or :v1.RowView.downloads!=:v2.RowView.downloads 
-	  or :v1.RowView.reads!=:v2.RowView.reads;
+create materialized view :v1.ChangedRowsM as 
+       select :v1.RowViewM.bibcode, :v1.RowViewM.id
+       from :v1.RowViewM,:v2.RowViewM
+       where :v1.RowViewM.bibcode=:v2.RowViewM.bibcode 
+          and :v1.RowViewM.authors!=:v2.RowViewM.authors 
+	  or :v1.RowViewM.refereed!=:v2.RowViewM.refereed 
+	  or :v1.RowViewM.simbad_objects!=:v2.RowViewM.simbad_objects 
+	  or :v1.RowViewM.grants!=:v2.RowViewM.grants 
+	  or :v1.RowViewM.citations!=:v2.RowViewM.citations 
+	  or :v1.RowViewM.boost!=:v2.rowViewM.boost 
+	  or :v1.RowViewM.norm_cites!=:v2.RowViewM.norm_cites 
+	  or :v1.RowViewM.citation_count!=:v2.RowViewM.citation_count 
+	  or :v1.RowViewM.read_count!=:v2.RowViewM.read_count 
+	  or :v1.RowViewM.readers!=:v2.RowViewM.readers 
+	  or :v1.RowViewM.downloads!=:v2.RowViewM.downloads 
+	  or :v1.RowViewM.reads!=:v2.RowViewM.reads;
 
 
 
