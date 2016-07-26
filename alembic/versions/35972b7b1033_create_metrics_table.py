7c696c52c9a881b733fc1fa78b1080e1ed4b2cc3
SpacemanSteve <Stephen.McDonald@cfa.harvard.edu>
2016-07-26 14:20:39 -0400

    #16 added alembic it has not yet been tested

---
+++ alembic/versions/35972b7b1033_create_metrics_table.py
@@ -0,0 +1,41 @@
+"""create metrics table
+
+Revision ID: 35972b7b1033
+Revises: 
+Create Date: 2016-07-26 14:11:35.655046
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '35972b7b1033'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'metrics',
+        sa.Column('id', Integer, primary_key=True),
+        sa.Column('bibcode', String, nullable=False, index=True, unique=True),
+        sa.Column('refereed',Boolean),
+        sa.Column('rn_citations', postgresql.REAL),
+        sa.Column('rn_citation_data', postgresql.JSON),
+        sa.Column('downloads', postgresql.ARRAY(Integer)),
+        sa.Column('reads', postgresql.ARRAY(Integer)),
+        sa.Column('an_citations', postgresql.REAL),
+        sa.Column('refereed_citation_num', Integer),
+        sa.Column('citation_num', Integer),
+        sa.Column('reference_num', Integer),
+        sa.Column('citations', postgresql.ARRAY(String)),
+        sa.Column('refereed_citations', postgresql.ARRAY(String)),
+        sa.Column('author_num', Integer),
+        sa.Column('an_refereed_citations', postgresql.REAL),
+        sa.Column('modtime', DateTime))
+
+
+def downgrade():
+    pass
