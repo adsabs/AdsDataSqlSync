c358d7e2a492eb08258b40243d977e118446ac78
SpacemanSteve <Stephen.McDonald@cfa.harvard.edu>
2016-11-15 10:48:15 -0500

    #20 initial code to create vhost uses rest api since pike doesn't support vhost creation

--- queue.py
+++ queue.py
@@ -2,6 +2,8 @@
 import pika, json
 import logging
 
+from urlparse import urlparse
+import requests
 from sqlalchemy.sql import select
 
 import utils
@@ -13,7 +15,7 @@ class Queue():
     This code puts all changed bibcodes on the queue.  Perhaps it
     could check to see of any Solr-related data changed, but it does not.
 
-    A single Postgres database holds all the bibcodes that hvae changed, 
+    A single Postgres database holds all the bibcodes that have changed, 
     one bibcode per row.  It does not store which columns have changed.
     """
 
@@ -69,16 +71,50 @@ class Queue():
         connection.close()
 
     def init_rabbitmq(self):
-        """useful for testing to create exchanges and routes on new rabbitmq instance"""
+        """useful for testing to create exchanges and routes on new rabbitmq instance
+        this code is currently only run from the command line via init-queue
+        """
         #url = self.config.get('RABBITMQ_URL', 'amqp://admin:password@localhost:5672/ADSimportpipeline')
         url = self.config.get('RABBITMQ_URL', 'amqp://guest:guest@localhost:5672/ADSimportpipeline')
+        self.verify_vhost(url)
         connection = pika.BlockingConnection(pika.URLParameters(url))
         channel = connection.channel()
         exchange = self.config.get('RABBITMQ_EXCHANGE', 'MergerPipelineExchange')
         route = queue=self.config.get('RABBITMQ_ROUTE', 'SolrUpdateRoute')
         channel.exchange_declare(exchange=exchange)
         channel.queue_declare(queue=route)
-        channel.basic_publish(exchange='', routing_key=queue,
-                              body='hello, world')
+        #channel.basic_publish(exchange='', routing_key=queue,
+        #                      body='hello, world')
         connection.close()
-        print '!!'
+        print 'queue init complete'
+    
+
+    def verify_vhost(self, url):
+        """if the vhost does not exist, create it                                                                   
+        pika does not appear to support this so we use the rest api
+        vhost name should be only thing in the path section of the url
+        """
+        print 'received', url
+        parsed = urlparse(url)
+        # generate url for rest request vhost info, rest request must use http
+        vhost_name = parsed.path
+        u = 'http://' + parsed.netloc + '/api/vhosts' + vhost_name
+        print 'u', u
+        r = requests.get(u)
+        if r.text[0] == '{':
+            # vhost exists and json response holds details
+            self.logger.info('{} vhost exists: {}'.format(vhost_name, parsed.text))
+            print 'vhost already exists'
+        elif '404 Not Found' in r.text:
+            # here with html response, we need to create the vhost
+            # send same url with a put to create
+            self.logger.info('{} vhost does not exist, trying to create'.format(vhost_name))
+            headers = {'Content-type': 'application/json'}
+            r = requests.put(u, headers=headers)
+            self.logger.info('{} created response = {} {}'.format(vhost_name, r, r.text))
+            print 'created vhost {}, {}'.format(vhost_name, r.text)
+        else:
+            self.logger.error('in verify_vhost, could not parse RabbitMQ reponse to vhost check: {}'.format(r))
+            print 'did not understand response', r
+
+
