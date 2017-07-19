a7cf9c3c57b1f1a89fc5dfafdade10d18759c01f
Roman Chyla <roman.chyla@gmail.com>
2017-07-19 17:41:04 -0400

    Fixed the alembic

--- alembic/versions/35972b7b1033_create_metrics_table.py
+++ alembic/versions/35972b7b1033_create_metrics_table.py
@@ -14,27 +14,28 @@ depends_on = None
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
 
 def upgrade():
     op.create_table(
         'metrics',
-        sa.Column('id', Integer, primary_key=True),
-        sa.Column('bibcode', String, nullable=False, index=True, unique=True),
-        sa.Column('refereed',Boolean),
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('bibcode', sa.String, nullable=False, index=True, unique=True),
+        sa.Column('refereed',sa.Boolean),
         sa.Column('rn_citations', postgresql.REAL),
         sa.Column('rn_citation_data', postgresql.JSON),
-        sa.Column('downloads', postgresql.ARRAY(Integer)),
-        sa.Column('reads', postgresql.ARRAY(Integer)),
+        sa.Column('downloads', postgresql.ARRAY(sa.Integer)),
+        sa.Column('reads', postgresql.ARRAY(sa.Integer)),
         sa.Column('an_citations', postgresql.REAL),
-        sa.Column('refereed_citation_num', Integer),
-        sa.Column('citation_num', Integer),
-        sa.Column('reference_num', Integer),
-        sa.Column('citations', postgresql.ARRAY(String)),
-        sa.Column('refereed_citations', postgresql.ARRAY(String)),
-        sa.Column('author_num', Integer),
+        sa.Column('refereed_citation_num', sa.Integer),
+        sa.Column('citation_num', sa.Integer),
+        sa.Column('reference_num', sa.Integer),
+        sa.Column('citations', postgresql.ARRAY(sa.String)),
+        sa.Column('refereed_citations', postgresql.ARRAY(sa.String)),
+        sa.Column('author_num', sa.Integer),
         sa.Column('an_refereed_citations', postgresql.REAL),
-        sa.Column('modtime', DateTime))
+        sa.Column('modtime', sa.DateTime))
 
 
 def downgrade():
