3f0b31f23ec9f9e232ba21221b73d4736ebde371
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2020-03-27 08:41:39 -0400

    Fixed manual delta test

--- tests/deltaTest.sh
+++ tests/deltaTest.sh
@@ -1,27 +1,53 @@
-
+#!/bin/bash
+set -e
 # test nonbib data ingest using historical data
+# - data1 directory contains historical data
+# - data2 directory is a copy of data1 with some changes
+#   changed bibcodes:
+#   - 1057wjlf.book.....C
+#   - 1905PhRvI..21..247N
+#   - 1950RPPh...13...24G
+#   - 2008arXiv0802.0143H
 
-# data1 directory contains historical data
-# data2 directory is a copy of data1 with some changes
-
-# de novo ingest data1 to use as baseline
-# delta ingest data2 and verify sql table of changed bibcodes is correct
-
-# de novo
-python ../app.py --dataPath `pwd`/data/data1/ --rowViewSchemaName nonbibtest2 runRowViewPipeline
-
-# delta with same data, should be no differences
-python ../app.py --dataPath `pwd`/data/data1/ --rowViewSchemaName nonbibtest2 --rowViewBaselineSchemaName baselinetest2 runRowViewPipelineDelta
-
-echo the above number of differences should all be 0
+echo "Do you wish to remove the test schemas and their content? (recommended: Yes)"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) python ../run.py dropIngestTables --rowViewSchemaName nonbibtest2 && python ../run.py dropIngestTables --rowViewSchemaName baselinetest2; break;;
+        No ) exit;;
+    esac
+done
 echo -e "\n"
 
-# delta with slightly different data
-python ../app.py --dataPath `pwd`/data/data2/ --rowViewSchemaName nonbibtest2 --rowViewBaselineSchemaName baselinetest2 runRowViewPipelineDelta
-echo the above number of differences should be 3 for bibcodes and 1 for everything else
+export DATA_PATH='./data/data1/'
 
-# verify table of changed bibcodes is correct
+echo "Proceed to load 'data1' and execute a delta with itself? (recommended: Yes)"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) python ../run.py --rowViewSchemaName nonbibtest2 runRowViewPipeline && python ../run.py --rowViewSchemaName nonbibtest2 --rowViewBaselineSchemaName baselinetest2 runRowViewPipelineDelta; break;;
+        No ) exit;;
+    esac
+done
 
-# finally, cleanup
-#python../app.py --dropIngestTables --rowViewSchemaName nonbibtest2
-#python../app.py --dropIngestTables --rowViewSchemaName baselinetest2
+echo ">> The above number of differences should all be 0"
+echo -e "\n"
+
+export DATA_PATH='./data/data2/'
+
+echo "Proceed to load 'data2' and execute a delta with 'data1'? (recommended: Yes)"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) python ../run.py --rowViewSchemaName nonbibtest2 --rowViewBaselineSchemaName baselinetest2 runRowViewPipelineDelta; break;;
+        No ) exit;;
+    esac
+done
+
+echo ">> the above number of differences should be 4 for bibcodes and 1 for everything else"
+echo -e "\n"
+
+echo "Do you wish to remove the test schemas and their content? (recommended: Yes)"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) python ../run.py dropIngestTables --rowViewSchemaName nonbibtest2&& python ../run.py dropIngestTables --rowViewSchemaName baselinetest2; break;;
+        No ) exit;;
+    esac
+done
