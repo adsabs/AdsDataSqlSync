5853f5398d29cbf82ace33749015332af3e5a7de
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2020-04-16 22:16:22 -0400

    Adapted unit tests

---
+++ tests/fix.sh
@@ -0,0 +1,20 @@
+set -e
+mkdir ./data/data1/links/nonarticle/ && touch ./data/data1/links/nonarticle/all.links
+mkdir ./data/data1/links/electr/ && touch ./data/data1/links/electr/all.links
+mkdir ./data/data1/links/pub_pdf/ && touch ./data/data1/links/pub_pdf/all.links
+mkdir ./data/data1/links/ads_pdf/ && touch ./data/data1/links/ads_pdf/all.links
+mkdir ./data/data1/links/eprint_pdf/ && touch ./data/data1/links/eprint_pdf/all.links
+mkdir ./data/data1/links/author_html/ && touch ./data/data1/links/author_html/all.links
+mkdir ./data/data1/links/ads_scan/ && touch ./data/data1/links/ads_scan/all.links
+mkdir ./data/data1/links/library/ && touch ./data/data1/links/library/all.links
+mkdir ./data/data1/links/spires/ && touch ./data/data1/links/spires/all.links
+
+mkdir ./data/data2/links/nonarticle/ && touch ./data/data2/links/nonarticle/all.links
+mkdir ./data/data2/links/electr/ && touch ./data/data2/links/electr/all.links
+mkdir ./data/data2/links/pub_pdf/ && touch ./data/data2/links/pub_pdf/all.links
+mkdir ./data/data2/links/ads_pdf/ && touch ./data/data2/links/ads_pdf/all.links
+mkdir ./data/data2/links/eprint_pdf/ && touch ./data/data2/links/eprint_pdf/all.links
+mkdir ./data/data2/links/author_html/ && touch ./data/data2/links/author_html/all.links
+mkdir ./data/data2/links/ads_scan/ && touch ./data/data2/links/ads_scan/all.links
+mkdir ./data/data2/links/library/ && touch ./data/data2/links/library/all.links
+mkdir ./data/data2/links/spires/ && touch ./data/data2/links/spires/all.links
---
+++ tests/manualTest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#python ../run.py dropIngestTables
+#DATA_PATH="./data/data1/" python ../run.py runRowViewPipeline
+#DATA_PATH="./data/data2/" python ../run.py runRowViewPipelineDelta
+#python ../run.py nonbibToMasterPipeline
+#python ../run.py nonbibDeltaToMasterPipeline
+#python ../run.py nonbibToMasterPipeline --bibcode 2008PhRvB..77q2503J,2006PhRvL..97z0801M,1905PhRvI..21..247N,1950RPPh...13...24G,1057wjlf.book.....C,2008arXiv0802.0143H
+#python ../run.py nonbibToMasterPipeline --filename bibcodes.list
